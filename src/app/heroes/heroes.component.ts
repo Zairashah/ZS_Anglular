3935ff35a0792e88b33721658675738b423f8b79
Zairashah <zairashah6@gmail.com>
2022-03-25 21:57:39 +0530

    Initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { verifyHostBindings } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero | undefined;
+
+  constructor() { }
+
+  ngOnInit() {}
+  onSelect(hero: Hero) {
+  this.selectedHero = hero;
+  console.log(this.selectedHero);
+}
+  }
